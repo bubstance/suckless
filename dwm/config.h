96bfbb57e17b99dd53020714550b05a47a1ff9ea
Kelly Roberts <kelly.glenn.roberts@gmail.com>
2023-02-19 16:28:31 -0700

    add windowmap patch for fading between tags with picom

--- dwm/config.h
+++ dwm/config.h
@@ -61,7 +61,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "X" };
 
 static const Rule rules[] = {
 	/* xprop(1):
