0931e3196a87e6e71c616739602981d17fe617cc
Kelly Roberts <kelly.glenn.roberts@gmail.com>
2023-02-26 21:09:48 -0700

    add test solarized-dark theme

---
+++ dwm/themes/solarized-dark.h
@@ -0,0 +1,11 @@
+static const char black[]  = "#073642";
+static const char white[]  = "#fdf6e3";
+static const char gray1[]  = "#002b36";
+static const char gray2[]  = "#586e75";
+static const char gray3[]  = "#93a1a1";
+static const char red[]    = "#dc322f";
+static const char orange[] = "#cb4b16";
+static const char yellow[] = "#b58900";
+static const char green[]  = "#859900";
+static const char blue[]   = "#268bd2";
+
